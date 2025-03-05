222dcd7cf7255784f77b91a5c9d808820bb21a24
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2025-03-05 12:07:33 -0500

    added about page with temp headshot

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,6 +4,13 @@ const About = () => {
   return (
     <div>
       <h1>Sarah Marchioni</h1>
+      <img
+        src="https://cdn.cosmos.so/5b385dc9-9a33-4f50-b081-3a358e920bc5?format=webp&w=1168"
+        className="img-fluid"
+        width={250} //TODO size headshot programmatically (adjustable by screen size)
+        height={250}
+        alt="A weathered painting of an old woman in furs with a pearl necklace"
+      ></img>
       <p>A queen a goddess {loremIpsum}</p>
     </div>
   );
