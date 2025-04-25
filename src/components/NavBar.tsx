3b996e15a2faae1e7db241b803b4efec7dc578eb
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2025-04-25 10:08:11 -0400

    updated packages

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,32 +8,22 @@ interface NavBarProps {
 
 const NavBar = ({ branding, pages }: NavBarProps) => {
   return (
-    // <div className="container">
     <nav className="navbar-expand-sm bg-body-tertiary fs-5">
-      {/* <div className="container-fluid"> */}
       <div className="navbar" id="navbarSupportedContent">
-        {/* <a className="navbar-brand" href="/">
-          {branding.displayName}
-        </a> */}
         <Link className="navbar-brand" to="/">
           {branding.displayName}
         </Link>
         <ul className="navbar-nav me-auto mb-1 mb-lg-0">
           {pages.map(({ displayName, link = "/" }, index) => (
             <li className="nav-item" key={index}>
-              {/* <a className="nav-link active" aria-current="page" href={link}>
-                {displayName}
-              </a> */}
               <Link className="nav-link active" aria-current="page" to={link}>
                 {displayName}
               </Link>
             </li>
           ))}
         </ul>
-        {/* </div> */}
       </div>
     </nav>
-    // </div>
   );
 };
 
